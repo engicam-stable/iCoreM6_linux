13c8c50ee063093612165a3d1a988e7513560988
Matteo Lisi <matteo.lisi@engicam.com>
2013-07-26 11:05:54 +0200

    removed .tmp files from git

--- .missing-syscalls.d
+++
@@ -1,6 +0,0 @@
--:  include/generated/autoconf.h \
- /home/user/kernel_for_wf111/linux-3.0.35.eng/arch/arm/include/asm/types.h \
- include/asm-generic/int-ll64.h \
- /home/user/kernel_for_wf111/linux-3.0.35.eng/arch/arm/include/asm/bitsperlong.h \
- include/asm-generic/bitsperlong.h \
- /home/user/kernel_for_wf111/linux-3.0.35.eng/arch/arm/include/asm/unistd.h
