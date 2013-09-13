b30f173a0bc0012492c8b552dc14d72d06555269
Matteo Manetti <matteo.manetti@engicam.com>
2013-09-13 16:13:28 +0200

    Add autodetect cpu core on makefile

--- build_linux.sh
+++ build_linux.sh
@@ -2,4 +2,5 @@
 export ARCH=arm
 export CROSS_COMPILE=/opt/freescale/usr/local/gcc-4.6.2-glibc-2.13-linaro-multilib-2011.12/fsl-linaro-toolchain/bin/arm-none-linux-gnueabi- 
 export PATH=$PATH:/opt/freescale/usr/local/gcc-4.6.2-glibc-2.13-linaro-multilib-2011.12/fsl-linaro-toolchain/bin/
-make -j 4 uImage
+echo `getconf _NPROCESSORS_ONLN`
+make -j`getconf _NPROCESSORS_ONLN` uImage
