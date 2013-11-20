6a0e941d450a412c6be1b3142fb074c30cb26844
Matteo Manetti <matteo.manetti@engicam.com>
2013-11-20 15:20:57 +0100

    LVDS power only for fb0

--- drivers/video/mxc/mxc_ipuv3_fb.c
+++ drivers/video/mxc/mxc_ipuv3_fb.c
@@ -1362,7 +1362,8 @@ static int mxcfb_blank(int blank, struct fb_info *info)
 	case FB_BLANK_VSYNC_SUSPEND:
 	case FB_BLANK_HSYNC_SUSPEND:
 	case FB_BLANK_NORMAL:
-		mx6q_icore_lvds_power(false);	
+		if(info->node==0)
+			mx6q_icore_lvds_power(false);	
 		if (mxc_fbi->dispdrv && mxc_fbi->dispdrv->drv->disable)
 			mxc_fbi->dispdrv->drv->disable(mxc_fbi->dispdrv);
 		ipu_disable_channel(mxc_fbi->ipu, mxc_fbi->ipu_ch, true);
@@ -1374,7 +1375,8 @@ static int mxcfb_blank(int blank, struct fb_info *info)
 		info->var.activate = (info->var.activate & ~FB_ACTIVATE_MASK) |
 				FB_ACTIVATE_NOW | FB_ACTIVATE_FORCE;
 		ret = mxcfb_set_par(info);
-		mx6q_icore_lvds_power(true);
+		if(info->node==0)
+			mx6q_icore_lvds_power(true);
 		break;
 	}
 	if (!ret)
