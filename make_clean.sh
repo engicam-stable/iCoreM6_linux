47bb5491efe2a49793ebf17db96bf1b6d50d332e
Matteo Manetti <matteo.manetti@engicam.com>
2013-12-27 14:34:33 +0100

    Add new menu config for iCore and RQS board

---
+++ make_clean.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export ARCH=arm
+export CROSS_COMPILE=/opt/freescale/usr/local/gcc-4.6.2-glibc-2.13-linaro-multilib-2011.12/fsl-linaro-toolchain/bin/arm-none-linux-gnueabi- 
+export PATH=$PATH:/opt/freescale/usr/local/gcc-4.6.2-glibc-2.13-linaro-multilib-2011.12/fsl-linaro-toolchain/bin/
+make clean
