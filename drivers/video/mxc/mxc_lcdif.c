1bb48e4830b9ccac487947180eb0f251dbeacecf
Domenico Acri <domenico.acri@engicam.com>
2014-02-24 15:27:54 +0100

    add support for LDB-WSGA display

--- drivers/video/mxc/mxc_lcdif.c
+++ drivers/video/mxc/mxc_lcdif.c
@@ -80,6 +80,12 @@ static struct fb_videomode lcdif_modedb[] = {
 	FB_SYNC_CLK_LAT_FALL | FB_SYNC_COMP_HIGH_ACT | FB_SYNC_VERT_HIGH_ACT,
 	FB_VMODE_NONINTERLACED
          },
+	{
+	/* 1024x600 @ 60 Hz */
+	"LDB-WSGA", 60, 1024, 600, 18939, 353, 47, 39, 4, 8, 2,
+	FB_SYNC_CLK_LAT_FALL | FB_SYNC_COMP_HIGH_ACT | FB_SYNC_VERT_HIGH_ACT,
+	FB_VMODE_NONINTERLACED
+	 },
 };
 static int lcdif_modedb_sz = ARRAY_SIZE(lcdif_modedb);
 
